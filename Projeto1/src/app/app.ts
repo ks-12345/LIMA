10a6eaa4cc4c4e5f7e0d3c33be131dd2b8c8896d
ks-12345 <brendaalvesri1234@gmail.com>
2025-10-30 16:30:47 -0300

    30-10-25

--- Projeto1/src/app/app.ts
+++ Projeto1/src/app/app.ts
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { 
-  protected readonly title = signal('meu-app');
+export class App {
+  protected readonly title = signal('Projeto1');
 }
